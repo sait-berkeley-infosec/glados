29ffdf0c677ab5d4c6d6857658bb47c1270e73e9
Nicholas Wold <nickw254@gmail.com>
2014-06-11 16:05:22 -0700

    Added some statements

--- audits/bin/break_things.sh
+++ audits/bin/break_things.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 # Break Things!
 # NWOLD SMASH.
 # Automates the process of fetching and translating
@@ -8,13 +8,16 @@ JAIL="/srv/sftpjail/home/audit"
 RESULTS="results-$(date +%F).jtr"
 JOHN="/opt/jtr"
 
+echo "NWOLD SMASH!"
 if [ -f "$JAIL/*.ldif" ]
+    echo "Dump files found!"
     then
         # Move all LDAP dumps from the jail to the dump folder.
         mv "$JAIL/*.ldif" "$ROOT/dumps/"
         # Translate each file and append the results to a text file.
         for file in "$ROOT/dumps/*.ldif"
         do
+            echo "Translating ${file}..."
             "$ROOT/bin/translate.sh" ${file} >> "$ROOT/tmp/$RESULTS"
         done
         #"$JOHN/run/john" "$ROOT/tmp/$RESULTS" PUT MORE COMMAND LINE STUFF HERE
